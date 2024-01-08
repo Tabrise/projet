f9734eb25340a24c724c24171f05aad8641d728f
MathieuGAY21 <m.gay@standardenligne.fr>
2024-01-08 11:17:37 +0100

    add custom page

--- routes/option.js
+++ routes/option.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const route = express.Router()
-const optionController = require('../controller/option')
+const options = require('../controller/option')
 
 route.get('/mois',)
+route.get('/options', options.AllOptions)
 module.exports = route
